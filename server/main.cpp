baa6310975a2bbdddc3318a8907903280e421f8e
bathingsoap <wong.alex1121@gmail.com>
2018-02-09 01:46:55 -0800

    Added calculations and stuff in main.
    Send to client: "aaa_bbb_ccc_ddd_ee", where aaa is ball x, bbb is ball y, ccc is player x, ddd is player y, and ee is player score.

--- server/main.cpp
+++ server/main.cpp
@@ -1,37 +1,46 @@
 #include <stdlib.h>
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <sstream>
 #include <time.h>
 #include "websocket.h"
 #include <cstdlib>
 #include <thread>
+#include <cmath>
 #define NUM_THREADS 4
 using namespace std;
 int count;
-float xPos;
-float yPos;
+std::pair<int, int> canvas;
+struct Ball {
+	int width = 10;
+	int height = 10;
+	int posX;
+	int posY;
+	int velocityX = 0;
+	int velocityY = 0;
+};
 
-webSocket server1;
+Ball ball;
+struct Paddle {
+	int width = 100;
+	int height = 5;
+	int posX;
+	int posY;
+	int speed = 4;
+	int score = 0;
+};
+
+Paddle player1;
+webSocket server;
 /* called when a client connects */
 void openHandler(int clientID) {
-	vector<int> clientIDs = server1.getClientIDs();
-	int a = clientIDs.size();
-	//server1.wsSend(clientIDs[0], std::to_string(a));
+	vector<int> clientIDs = server.getClientIDs();
 	if (clientIDs.size() > 1) {
-		for (int i = 2; i < clientIDs.size(); i++) {
-			server1.wsClose(i);
+		for (int i = 1; i < clientIDs.size(); i++) {
+			server.wsClose(i);
 		}
 	}
-    /*ostringstream os;
-    os << "Stranger " << clientID << " has joined.";
-
-    vector<int> clientIDs = server1.getClientIDs();
-    for (int i = 0; i < clientIDs.size(); i++){
-        if (clientIDs[i] != clientID)
-            server1.wsSend(clientIDs[i], os.str());
-    }
-    server1.wsSend(clientID, "Welcome!");*/
 }
 
 /* called when a client disconnects */
@@ -39,10 +48,10 @@ void closeHandler(int clientID){
     ostringstream os;
     //os << "Stranger " << clientID << " has leaved.";
 
-    vector<int> clientIDs = server1.getClientIDs();
+    vector<int> clientIDs = server.getClientIDs();
     for (int i = 0; i < clientIDs.size(); i++){
         if (clientIDs[i] != clientID)
-            server1.wsSend(clientIDs[i], os.str());
+            server.wsSend(clientIDs[i], os.str());
     }
 }
 
@@ -51,18 +60,72 @@ void messageHandler(int clientID, string message){
     ostringstream os;
     //os << "Stranger " << clientID << " says: " << message;
 
-    vector<int> clientIDs = server1.getClientIDs();
-    for (int i = 0; i < clientIDs.size(); i++){
-        if (clientIDs[i] != clientID)
-            server1.wsSend(clientIDs[i], os.str());
-    }
+    vector<int> clientIDs = server.getClientIDs();
+	if (message == "l") {
+		player1.posX = fmin(0, player1.posX - player1.speed);
+	}
+	if (message == "r") {
+		player1.posX = fmax(canvas.first-player1.posX, player1.posX + player1.speed);
+	}
+
 }
 
 /* called once per select() loop */
-void periodicHandler(){
-    static time_t next = time(NULL) + (float)0.3;
-    time_t current = time(NULL);
-    if (current >= next){
+void periodicHandler() {
+	static time_t next = time(NULL) + (float)0.3;
+	time_t current = time(NULL);
+	if (current >= next) {
+		ball.posX += ball.velocityX;
+		ball.posY += ball.velocityY;
+		if ((ball.velocityX < 0 && ball.posX < 0) ||
+			(ball.velocityX > 0 && ball.posX + ball.width > canvas.first)) {
+			ball.velocityX = -ball.velocityX;
+		}
+
+		//bottom and top
+		if (ball.velocityY < 0)
+		{
+			//top
+			if (ball.posY < 0)
+				ball.velocityY = -ball.velocityY;
+		}
+		else
+		{
+			//bottom
+			if (player1.posY <= ball.posY + ball.height)
+				//  && this.p1.y > this.ball.y - this.ball.vy + this.ball.height)
+			{
+				//collision distance
+				int d = ball.posY + ball.height - player1.posY;
+				//time
+				int t = d / ball.velocityY;
+				//x translate
+				int x = ball.velocityX*t + (ball.posX - ball.velocityX);
+				if (x >= player1.posX && x + ball.width <= player1.posX + player1.width)
+				{
+					ball.velocityY = -ball.velocityY;
+					player1.score++;
+				}
+			}
+			if (ball.posY > canvas.second)
+				ball.velocityY = -ball.velocityY;
+		}
+
+		ostringstream os;
+		os << setfill('0') << setw(3) << ball.posX << "_";
+		os << setfill('0') << setw(3) << ball.posY << "_";
+		os << setfill('0') << setw(3) << player1.posX<< "_";
+		os << setfill('0') << setw(3) << player1.posY<< "_";
+		os << setfill('0') << setw(2) << player1.score;
+	vector<int> clientIDs = server.getClientIDs();
+	for (int i = 0; i < clientIDs.size(); i++){
+		server.wsSend(clientIDs[i], os.str());
+	}
+	next = time(NULL) + (float)0.3;
+	cout << os.str()<<std::endl;
+	}
+	
+		/*
         ostringstream os;
 		//Deprecated ctime API in Windows 10
 		char timecstring[26];
@@ -71,32 +134,39 @@ void periodicHandler(){
         timestring = timestring.substr(0, timestring.size() - 1);
         os << timestring;
 
-        vector<int> clientIDs = server1.getClientIDs();
+        vector<int> clientIDs = server.getClientIDs();
         for (int i = 0; i < clientIDs.size(); i++)
-            server1.wsSend(clientIDs[i], os.str());
+            server.wsSend(clientIDs[i], os.str());
+		
+        next = time(NULL) + (float)0.03;
+    }*/
+}
 
-        next = time(NULL) + (float)0.3;
-    }
-}
-void ss1(int port) {
-	server1.setOpenHandler(openHandler);
-	server1.setCloseHandler(closeHandler);
-	server1.setMessageHandler(messageHandler);
-	server1.setPeriodicHandler(periodicHandler);
-	server1.startServer(port);
-}
 
 int main(int argc, char *argv[]){
-	int port;
-    cout << "Please set server port: ";
-    cin >> port;
-	ss1(port);
+
+	canvas.first = 600;
+	canvas.second = 500;
+	//Ball ball;
+	ball.posX = canvas.first / 2;
+	ball.posY = canvas.second / 2;
+	ball.velocityX = 5;
+	ball.velocityY = 5;
+	//Paddle player1;
+	player1.posX = canvas.first / 2;
+	player1.posY = canvas.second- 25;
+
+
 	//port1 = 1000;
     /* set event handler */
 
 	
     //server.setPeriodicHandler(periodicHandler);
-
+	server.setOpenHandler(openHandler);
+	server.setCloseHandler(closeHandler);
+	server.setMessageHandler(messageHandler);
+	server.setPeriodicHandler(periodicHandler);
+	server.startServer(1000);
     /* start the chatroom server, listen to ip '127.0.0.1' and port '8000' */
 
     return 1;
