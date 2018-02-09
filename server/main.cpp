644013a3cbfa8fca924120a9081afaecbdcc8b9f
Fang <fang@dhcp-v059-237.mobile.uci.edu>
2018-02-08 17:42:08 -0800

    new branch for client

--- server/main.cpp
+++ server/main.cpp
@@ -4,100 +4,89 @@
 #include <sstream>
 #include <time.h>
 #include "websocket.h"
-#include <cstdlib>
-#include <thread>
-#define NUM_THREADS 4
-using namespace std;
-int count;
-float xPos;
-float yPos;
 
-webSocket server1;
+using namespace std;
+
+webSocket server;
+
 /* called when a client connects */
-void openHandler(int clientID) {
-	vector<int> clientIDs = server1.getClientIDs();
-	int a = clientIDs.size();
-	//server1.wsSend(clientIDs[0], std::to_string(a));
-	if (clientIDs.size() > 1) {
-		for (int i = 2; i < clientIDs.size(); i++) {
-			server1.wsClose(i);
-		}
-	}
-    /*ostringstream os;
+void openHandler(int clientID){
+    ostringstream os;
     os << "Stranger " << clientID << " has joined.";
 
-    vector<int> clientIDs = server1.getClientIDs();
+    vector<int> clientIDs = server.getClientIDs();
     for (int i = 0; i < clientIDs.size(); i++){
         if (clientIDs[i] != clientID)
-            server1.wsSend(clientIDs[i], os.str());
+            server.wsSend(clientIDs[i], os.str());
     }
-    server1.wsSend(clientID, "Welcome!");*/
+    server.wsSend(clientID, "Welcome!");
 }
 
 /* called when a client disconnects */
 void closeHandler(int clientID){
     ostringstream os;
-    //os << "Stranger " << clientID << " has leaved.";
+    os << "Stranger " << clientID << " has leaved.";
 
-    vector<int> clientIDs = server1.getClientIDs();
-    for (int i = 0; i < clientIDs.size(); i++){
-        if (clientIDs[i] != clientID)
-            server1.wsSend(clientIDs[i], os.str());
-    }
+    // vector<int> clientIDs = server.getClientIDs();
+    // for (int i = 0; i < clientIDs.size(); i++){
+    //     if (clientIDs[i] != clientID)
+    //         server.wsSend(clientIDs[i], os.str());
+    // }
+
+    vector<int> clientIDs = server.getClientIDs();
+	if (clientIDs.size() > 1) {
+		for (int i = 1; i < clientIDs.size(); i++) {
+			server.wsClose(i);
+		}
+	}
 }
+
 
 /* called when a client sends a message to the server */
 void messageHandler(int clientID, string message){
     ostringstream os;
-    //os << "Stranger " << clientID << " says: " << message;
+    // os << "Stranger " << clientID << " says: " << message;
+    os << message;
 
-    vector<int> clientIDs = server1.getClientIDs();
+    vector<int> clientIDs = server.getClientIDs();
     for (int i = 0; i < clientIDs.size(); i++){
-        if (clientIDs[i] != clientID)
-            server1.wsSend(clientIDs[i], os.str());
+        // if (clientIDs[i] != clientID)
+            server.wsSend(clientIDs[i], os.str());
     }
 }
 
 /* called once per select() loop */
 void periodicHandler(){
-    static time_t next = time(NULL) + (float)0.3;
+    static time_t next = time(NULL) + 10;
     time_t current = time(NULL);
     if (current >= next){
         ostringstream os;
-		//Deprecated ctime API in Windows 10
-		char timecstring[26];
-		ctime_s(timecstring, sizeof(timecstring), &current);
-		string timestring(timecstring);
+        string timestring = ctime(&current);
         timestring = timestring.substr(0, timestring.size() - 1);
         os << timestring;
 
-        vector<int> clientIDs = server1.getClientIDs();
+        vector<int> clientIDs = server.getClientIDs();
         for (int i = 0; i < clientIDs.size(); i++)
-            server1.wsSend(clientIDs[i], os.str());
+            server.wsSend(clientIDs[i], os.str());
 
-        next = time(NULL) + (float)0.3;
+        next = time(NULL) + 10;
     }
-}
-void ss1(int port) {
-	server1.setOpenHandler(openHandler);
-	server1.setCloseHandler(closeHandler);
-	server1.setMessageHandler(messageHandler);
-	server1.setPeriodicHandler(periodicHandler);
-	server1.startServer(port);
 }
 
 int main(int argc, char *argv[]){
-	int port;
-    cout << "Please set server port: ";
-    cin >> port;
-	ss1(port);
-	//port1 = 1000;
-    /* set event handler */
+    int port = 8000;
 
-	
+    // cout << "Please set server port: ";
+    // cin >> port;
+
+    /* set event handler */
+    server.setOpenHandler(openHandler);
+    server.setCloseHandler(closeHandler);
+    server.setMessageHandler(messageHandler);
     //server.setPeriodicHandler(periodicHandler);
 
     /* start the chatroom server, listen to ip '127.0.0.1' and port '8000' */
+    server.startServer(port);
 
     return 1;
 }
